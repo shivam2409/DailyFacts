a78eff93a1fa82d195e9a9bd2d68aa9ae70efdb9
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2020-06-20 18:04:26 -0400

    Clean

--- frontview/src/factList.js
+++ frontview/src/factList.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+// function FactList() {
+
+//   useEffect =(async()=>{
+
+//     const res = await axios.get('https://uselessfacts.jsph.pl/', {
+//    headers: { accept: 'application/json' },
+//   })
+//   return <h1>Daily Facts</h1>;
+// }
+
+// export default FactList;
 class Factlist extends Component {
   async componentDidMount() {
     //Faching data
-    const res = await axios.get(
-      'https://webknox-trivia-knowledge-facts-v1.p.rapidapi.com/trivia/random',
-      {
-        headers: {
-          accept: 'application/json',
-          'x-rapidapi-host': 'webknox-trivia-knowledge-facts-v1.p.rapidapi.com',
-          'x-rapidapi-key':
-            '61510d0b0dmsh7308c9ac76da14cp1e5883jsn7eadea2e874b',
-          useQueryString: true,
-        },
-      }
-    );
+    const res = await axios.get('https://api.football-data.org/v2/matches', {
+      // headers: { accept: 'application/json' },
+      method: 'GET',
+    });
     console.log(res);
   }
   render() {
